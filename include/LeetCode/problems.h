2c8ad8b6f9b026e6f20e4ea178b63d3567c37f28
Alexdragon737 <coman.alexandru88@gmail.com>
2023-11-19 19:06:12 +0200

    A new batch of LeetCode problems

--- include/LeetCode/problems.h
+++ include/LeetCode/problems.h
@@ -3,9 +3,19 @@
 #include <string>
 #include <algorithm>
 #include <map>
+#include <iostream>
 
 namespace LeetCode
 {
+    // LeetCode style linked list node
+    struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
     class Solution 
     {
         public:
@@ -245,6 +255,111 @@ namespace LeetCode
                 return false;
         }
         return true;
+    }
+
+        // Problem: https://leetcode.com/problems/merge-sorted-array/
+        void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n) {
+            for(int i=0;i<n;i++)
+                nums1[m+i] = nums2[i];
+
+            std::sort(nums1.begin(),nums1.end());
+        }
+
+        // Problem: https://leetcode.com/problems/majority-element/description/
+        int majorityElement(std::vector<int>& nums) {
+        std::map<int,int> apparitions;
+        int max = 0, ans = 0;
+        for(auto x : nums)
+        {
+            if(apparitions.find(x) == apparitions.end())
+                apparitions[x] = 1;
+            else apparitions[x]++;
+        }
+
+        for(auto it : apparitions)
+        {
+            if(it.second > max)
+            {
+                max = it.second;
+                ans = it.first;
+            }
+        }   
+        return ans; 
+    }
+
+         /*
+            Problem: https://leetcode.com/problems/pascals-triangle/
+
+            Props to community for the solution.
+            This uses the combinatorial formula C(n,k) = C(n-1,k-1) + C(n-1,k)
+
+            I really wanted to implement this using the binomial theorem, 
+            but factorials are a pain to implement without having a BigInt library,
+            since the regular data types are only able to cover up to n=15.
+        */
+        std::vector<std::vector<int>> generate(int numRows) {
+            std::vector<std::vector<int>> ans;
+            for(int i=0;i<numRows;i++)
+            {
+                std::vector<int> ansRow(i+1,1);
+                for(int j=1;j<i;j++)
+                {
+                    ansRow[j] = ans[i-1][j-1] + ans[i-1][j];
+                }
+                ans.push_back(ansRow);
+            }
+            return ans;
+        }
+
+        // Problem: https://leetcode.com/problems/number-of-1-bits/
+        int hammingWeight(uint32_t n) {
+            uint32_t zeroes = 0;
+
+            /*
+                We just apply the basic binary conversion algorithm.
+                When we find a '1' in the binary representation, the counter increments.
+            */
+            while(n != 0)
+            {
+                if(n % 2 != 0)
+                    zeroes++;
+                n = n/2;
+            }
+            return zeroes;
+        }
+
+        // Problem: https://leetcode.com/problems/merge-two-sorted-lists/
+        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        /*
+            When we're working with linked lists, and we have to
+            return the head of the new linked list, the easiest
+            approach is to just use two nodes.
+            One will be modified, the other will stay as is.
+        */
+        ListNode *list3 = new ListNode(), *dummy = list3;
+        
+        // This is what we need to do if both lists are not empty
+        while(list1 != NULL && list2 != NULL)
+        {
+            if(list1->val < list2->val){
+                dummy->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                dummy->next = list2;
+                list2 = list2->next;
+            }
+            dummy = dummy->next;
+        }
+
+        // What we do after we reach the end of the linked list
+        if(list1 != NULL) dummy->next = list1;
+        if(list2 != NULL) dummy->next = list2;
+
+        // Extra step to avoid leaks
+        return dummy = list3->next;
+        delete list3;
+        return dummy;
     }
 
         
